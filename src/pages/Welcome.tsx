911ef571958c78c70dacede7aa6ac90f3294213b
laism <laism@chinacsci.com>
2021-02-08 11:05:26 +0800

    update

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
-import { useIntl, FormattedMessage } from 'umi';
-import styles from './Welcome.less';
+import { Card, Alert } from 'antd';
+// import { useIntl, FormattedMessage } from 'umi';
+// import styles from './Welcome.less';
 
-const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+// const CodePreview: React.FC = ({ children }) => (
+//   <pre className={styles.pre}>
+//     <code>
+//       <Typography.Text copyable>{children}</Typography.Text>
+//     </code>
+//   </pre>
+// );
 
 export default (): React.ReactNode => {
-  const intl = useIntl();
   return (
     <PageContainer>
       <Card>
         <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: '更快更强的重型组件，已经发布。',
-          })}
+          message="欢迎使用商汇管理系统。"
           type="success"
           showIcon
           banner
@@ -30,7 +26,7 @@ export default (): React.ReactNode => {
             marginBottom: 24,
           }}
         />
-        <Typography.Text strong>
+        {/* <Typography.Text strong>
           <FormattedMessage id="pages.welcome.advancedComponent" defaultMessage="高级表格" />{' '}
           <a
             href="https://procomponents.ant.design/components/table"
@@ -56,7 +52,7 @@ export default (): React.ReactNode => {
             <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
           </a>
         </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+        <CodePreview>yarn add @ant-design/pro-layout</CodePreview> */}
       </Card>
     </PageContainer>
   );
